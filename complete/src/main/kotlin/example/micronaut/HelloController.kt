2530d6b1caddaf8825ddf9716976c0cd56e2fc9a
John Russ <j.kelly.russ@gmail.com>
2018-11-28 01:11:30 +0000

    Make it build

--- complete/src/main/kotlin/example/micronaut/HelloController.kt
+++ complete/src/main/kotlin/example/micronaut/HelloController.kt
@@ -8,9 +8,15 @@ import io.micronaut.http.annotation.Produces
 @Controller("/hello") // <1>
 class HelloController {
 
-    @Get("/")  // <2>
-    @Produces(MediaType.TEXT_PLAIN) // <3>
+    @Get("/")  //
+    @Produces(MediaType.TEXT_PLAIN)
     fun index(): String {
         return "Hello World"  // <4>
+    }
+
+    @Get("/dope")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun dopeIndex(): String {
+        return new Winner()
     }
 }
